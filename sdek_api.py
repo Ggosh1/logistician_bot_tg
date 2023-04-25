3fa4f62297e496d2cc81c931aca8453e8626bb69
Pandatokyo <99756016+Pandatokyo@users.noreply.github.com>
2023-04-25 22:49:39 +0300

    updated sdek_api

--- sdek_api.py
+++ sdek_api.py
@@ -1,17 +1,19 @@
 import json
 import requests
 import schedule
-
-url = 'https://api.edu.cdek.ru/v2/calculator/tariff'
-
-
-def update_token():
-    with open('config.txt', 'r') as c:
-        sdek_access_token = c.readline().strip('\n')
-        global sdek_acess_token
+import datetime
+from config import SDEK_CODES
+time_now = str(datetime.datetime.now())
+time_formatted = time_now[:10] + "T" + time_now[11:19] + "+0300"
 
 
 def get_token():
+    with open('config.txt', 'r') as c:
+        sdek_access_token = c.readline().strip('\n')
+        return sdek_access_token
+
+
+def update_token():
     url2 = 'https://api.edu.cdek.ru/v2/oauth/token?parameters'
     par = {'grant_type': 'client_credentials', 'client_id': 'EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI',
            'client_secret': 'PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG'}
@@ -23,39 +25,35 @@ def get_token():
     return
 
 
-#schedule.every(50).minutes.do(get_token)
+def get_city_code(city):
+    return SDEK_CODES[city]
 
 
-#data = json.dumps({
-#    "type": "2",
-#    "currency": "1",
-#    "tariff_code": "11",
-#    "from_location": {
-#        "code": 270
-#    },
-#    "to_location": {
-#        "code": 44
-#    },
-#    "services": [
-#        {
-#            "code": "CARTON_BOX_XS",
-#            "parameter": "2"
-#        }
-#    ],
-#    "packages": [
-#        {
-#            "height": 10,
-#            "length": 10,
-#            "weight": 4000,
-#            "width": 10
-#        }
-#    ]
-#})
+def parse_packages(height, width, length, weight, amount):
+    return [{"height": height, "length": length, "weight": weight, "width": width} for _ in range(amount)]
 
 
-#header = {"Authorization": f"Bearer {access_token}", 'Content-Type': 'application/json'}
-#out = requests.post(url, data=data, headers=header)
-#print(out.content)
-#
-#while True:
-#    schedule.run_pending()
+def get_info_delivery(from_location, to_location, *args):
+    calculator_url = 'https://api.edu.cdek.ru/v2/calculator/tarifflist'
+    packages = parse_packages(*args)
+    try:
+        code_from_location = get_city_code(from_location)
+        code_to_location = get_city_code(to_location)
+    except KeyError:
+        return f'Доставки в город {to_location}, либо доставки из города {from_location} нет'
+    j = {
+        "type": "2",
+        "currency": "1",
+        "from_location": {
+            "code": f'{code_from_location}'
+        },
+        "to_location": {
+            "code": f'{code_to_location}'
+        },
+        "packages": f'{packages}'
+    }
+    return requests.post(url=calculator_url, json=j)
+
+
+update_token()
+print(get_token())
